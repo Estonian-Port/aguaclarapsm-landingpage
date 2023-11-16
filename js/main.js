6e4d64dcd273c3509d7103dc37045896938bd18b
RdzSebastian <rdzsebastian@gmail.com>
2023-11-16 11:41:12 -0300

    Ajustes de librerias owl-carousel y imagenes

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    /*----------------------------------------------------*/
+    /*  Noticias owl-carousel
+    /*----------------------------------------------------*/
+        $('.owl-carousel').owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true
+        })
+})
